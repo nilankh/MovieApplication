ea766dbc51e5122980631e15c772ca30931cd01f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-10 08:37:49 +0530

    creating the store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import "./index.css";
+import App from "./components/App";
+import movies from './reducers'
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// it accepts an argument and we will pass an argument as a reducer
+const store = createStore(movies);
+console.log('store', store)
+console.log('store', store.getState())
 
+
+ReactDOM.render(<App />, document.getElementById("root"));
