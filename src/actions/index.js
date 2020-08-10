f31b55efac06de120c32a7ed8759c3ca39504b2f
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-10 09:41:44 +0530

    action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,17 @@
 // action is just a way to telling a store i want to do something
 
 // {
-    // type: 'ADD_MOVIES',
-    // movies: [m1, m2, m3]
+// type: 'ADD_MOVIES',
+// movies: [m1, m2, m3]
 // }
+
+// action types
+export const ADD_MOVIES = "ADD_MOVIES";
+
+// actions creators
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies
+  };
+}
--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,19 @@ import React from "react";
 import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
+import { addMovies } from "../actions";
 
 class App extends React.Component {
   componentDidMount() {
     const { store } = this.props;
     // we have to subsribe to make changes
-    this.forceUpdate();
+    this.forceUpdate();//forcefully updating the app component to render
     store.subscribe(() => {
       console.log("UPDATED");
     });
     // in real word wwe make api call
     // dispatch the action
-    store.dispatch({
-      type: "ADD_MOVIES",
-      movies: data,
-    });
+    store.dispatch(addMovies(data));
     console.log("STATE", this.props.store.getState());
   }
 
