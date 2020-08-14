dc7abc027d6399f126826ba85c1c39de0803cc6d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-14 08:48:36 +0530

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import "./index.css";
 import App from "./components/App";
 // import movies from "./reducers";
 import rootReducer from "./reducers";
 
+// function logger(obj, next, action)
+// logger(obj)(next)(action)
+// next is used to refer another middleware
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log("ACTION_TYPE =", action.type);
+      next(action);
+    };
+  };
+};
+
 // it accepts an argument and we will pass an argument as a reducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log("store", store);
 // console.log('BeforeState', store.getState())
 
