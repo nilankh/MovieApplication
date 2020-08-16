eb03b8496d03cdc79e58fa073e3786e338f87aac
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-16 07:39:54 +0530

    context api, Using Context API-!, Using Context API -2

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { data } from "../data";
 import Navbar from "./Navbar";
 import MovieCard from "./MovieCard";
 import { addMovies, setShowFavourite } from "../actions";
+import { StoreContext } from "../index";
 
 class App extends React.Component {
   componentDidMount() {
@@ -43,7 +44,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <Navbar dispatch={this.props.store.dispatch} search={search} />
+        <Navbar  search={search} />
         <div className="main">
           <div className="tabs">
             <div
@@ -78,4 +79,14 @@ class App extends React.Component {
   }
 }
 
-export default App;
+
+class AppWrapper extends React.Component{
+  render() {
+    return(
+      <StoreContext.Consumer >
+      {(store) => <App store={store} /> }
+    </StoreContext.Consumer>
+    )
+  }
+}
+export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext } from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
@@ -42,6 +42,20 @@ const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 console.log("store", store);
 // console.log('BeforeState', store.getState())
 
+export const StoreContext = createContext();
+
+console.log("StoreContext", StoreContext);
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    );
+  }
+}
 // //DISPATCHING THE ACTIONS
 // store.dispatch({
 //   type: 'ADD_MOVIES',
@@ -49,4 +63,9 @@ console.log("store", store);
 // })
 // console.log('AfterState', store.getState())
 
-ReactDOM.render(<App store={store} />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
